6ec0fea764b8d74fd87f324a3e4963971e8c85c8
usman <usmanshakeel43500@gmail.com>
2026-08-07 20:29:01 +0500

    change password functionality added

--- src/Controllers/ProfileController.php
+++ src/Controllers/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 use App\Contracts\RequestValidatorFactoryInterface;
 use App\DataObjects\UserProfileData;
+use App\RequestValidator\UpdatePasswordRequestValidator;
 use App\RequestValidator\UpdateProfileRequestValidator;
+use App\Services\PasswordResetService;
 use App\Services\UserProfileService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -14,7 +16,8 @@ class ProfileController
     public function __construct(
         private readonly Twig $twig,
         private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
-        private readonly UserProfileService $userProfileService
+        private readonly UserProfileService $userProfileService,
+        private readonly PasswordResetService $passwordResetService
     ) {
     }
     public function index(Request $request, Response $response): Response
@@ -37,6 +40,17 @@ class ProfileController
             $user,
             new UserProfileData($user->getEmail(), $data['name'], (bool) ($data['twoFactor'] ?? false))
         );
+        return $response;
+    }
+    public function updatePassword(Request $request, Response $response): Response
+    {
+        $user = $request->getAttribute('user');
+        $data = $this->requestValidatorFactory->make(UpdatePasswordRequestValidator::class)->validate(
+            $request->getParsedBody() + ['user' => $user]
+        );
+
+        $this->passwordResetService->updatePassword($user, $data['newPassword']);
+
         return $response;
     }
 }
